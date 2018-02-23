702b539dac79afcef0ef4d29b1a9459ff7af7189
Gabriel Fior <gabrielfior@Gabriels-MacBook-Pro.local>
2018-02-23 17:55:17 +0100

    lesson 3 chap 10

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -4,20 +4,24 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
-  // Start here
   modifier aboveLevel(uint _level, uint _zombieId) {
-        require(zombies[_zombieId].level >= _level);
-        _;
+    require(zombies[_zombieId].level >= _level);
+    _;
   }
 
-
-function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
-  require(msg.sender == zombieToOwner[_zombieId]);
-  zombies[_zombieId].name = _newName;
+  function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].name = _newName;
   }
 
-  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId){
-      require(msg.sender == zombieToOwner[_zombieId]);
-      zombies[_zombieId].dna = _newDna;
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].dna = _newDna;
   }
+
+  // Create your function here
+  function getZombiesByOwner(address _owner) external view returns (uint[]) {
+        
+  }
+
 }
