f28302062ac91e29b02db8441bf140524479eba0
Gabriel Fior <gabrielfior@Gabriels-MacBook-Pro.local>
2018-02-23 17:29:15 +0100

    lesson 3 chap 6

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -25,6 +25,16 @@ contract ZombieFeeding is ZombieFactory {
     kittyContract = KittyInterface(_address);
   }
 
+  // 1. Define `_triggerCooldown` function here
+  function _triggerCooldown(Zombie storage _zombie) internal {
+    _zombie.readyTime = uint32(now + cooldownTime);
+  }
+
+  // 2. Define `_isReady` function here
+  function _isReady(Zombie storage _zombie) internal view returns (bool) {
+    return (_zombie.readyTime <= now);
+  }
+
   function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
