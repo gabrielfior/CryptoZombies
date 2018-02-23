1aa0f06c00716fb6e55223a1ff5a96a0d5d7b190
Gabriel Fior <gabrielfior@Gabriels-MacBook-Pro.local>
2018-02-23 17:48:21 +0100

    lesson 3 chap 9

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -9,4 +9,15 @@ contract ZombieHelper is ZombieFeeding {
         require(zombies[_zombieId].level >= _level);
         _;
   }
+
+
+function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
+  require(msg.sender == zombieToOwner[_zombieId]);
+  zombies[_zombieId].name = _newName;
+  }
+
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId){
+      require(msg.sender == zombieToOwner[_zombieId]);
+      zombies[_zombieId].dna = _newDna;
+  }
 }
