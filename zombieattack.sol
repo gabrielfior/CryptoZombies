50e84199276a30a7c6b7e0ee0e4aafe2ea7dbdf8
Gabriel Fior <gabrielfior@Gabriels-MacBook-Pro.local>
2018-03-02 17:18:37 +0100

    lesson 4 chap 4

--- zombieattack.sol
+++ zombieattack.sol
@@ -3,5 +3,12 @@ pragma solidity ^0.4.19;
 import "./zombiehelper.sol";
 
 contract ZombieBattle is ZombieHelper {
+    uint randNonce = 0;
+
+    // random modulus
+    function randMod(uint _modulus) internal returns (uint) {
+        randNonce++;
+        return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
+    }
 
 }
