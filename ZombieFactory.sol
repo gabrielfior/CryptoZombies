b2e37949d1c9693fbfd2dbf49d2656c4d992487a
Gabriel Fior <gabrielfior@Gabriels-MacBook-Pro.local>
2018-03-02 16:47:41 +0100

    lesson 4 chap 1

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -8,15 +8,13 @@ contract ZombieFactory is Ownable {
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
-    // 1. Define `cooldownTime` here
     uint cooldownTime = 1 days;
 
-
     struct Zombie {
-        string name;
-        uint dna;
-        uint32 level;
-        uint32 readyTime;
+      string name;
+      uint dna;
+      uint32 level;
+      uint32 readyTime;
     }
 
     Zombie[] public zombies;
@@ -25,7 +23,6 @@ contract ZombieFactory is Ownable {
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string _name, uint _dna) internal {
-        // 2. Update the following line:
         uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
@@ -45,4 +42,3 @@ contract ZombieFactory is Ownable {
     }
 
 }
-
--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -33,11 +33,9 @@ contract ZombieFeeding is ZombieFactory {
       return (_zombie.readyTime <= now);
   }
 
-  // 1. Make this function internal
   function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
-    
     require(_isReady(myZombie));
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
@@ -45,7 +43,6 @@ contract ZombieFeeding is ZombieFactory {
       newDna = newDna - newDna % 100 + 99;
     }
     _createZombie("NoName", newDna);
-    // 3. Call `triggerCooldown`
     _triggerCooldown(myZombie);
   }
 
@@ -54,5 +51,4 @@ contract ZombieFeeding is ZombieFactory {
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
     feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
-
 }
