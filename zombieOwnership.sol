094a912ce6f55c50258546ca107b380713a66b5a
Gabriel Fior <gabriel.fior@d-fine.de>
2018-04-03 09:58:00 +0200

    lesson 5 chap 6

--- zombieOwnership.sol
+++ zombieOwnership.sol
@@ -14,15 +14,14 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
   }
 
   function _transfer(address _from, address _to, uint256 _tokenId) private {
-      ownerZombieCount[_to]++;
-      ownerZombieCount[_from]--;
-      zombieToOwner[_tokenId] = _to;
-      Transfer(_from, _to, _tokenId);
-
+    ownerZombieCount[_to]++;
+    ownerZombieCount[_from]--;
+    zombieToOwner[_tokenId] = _to;
+    Transfer(_from, _to, _tokenId);
   }
 
-  function transfer(address _to, uint256 _tokenId) public {
-
+  function transfer(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) {
+    _transfer(msg.sender, _to, _tokenId);
   }
 
   function approve(address _to, uint256 _tokenId) public {
